4b7712258b5068d3425b8550ee9586e8195574db
Ali_Kookani <alikookani@gmail.com>
2024-05-26 12:07:46 +0430

    F1 score added

--- shopping.py
+++ shopping.py
@@ -4,7 +4,8 @@ import calendar
 
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix
+from ManualNearestNeighbor import KNearestNeighbors
+#from sklearn.metrics import confusion_matrix
 
 TEST_SIZE = 0.4
 DEFAULT_FILE_NAME = 'shopping.csv'
@@ -25,15 +26,23 @@ def main():
     )
 
     # Train model and make predictions
-    model = train_model(X_train, y_train)
-    predictions = model.predict(X_test)
-    sensitivity, specificity = evaluate(y_test, predictions)
+    model, MyModel = train_model(X_train, y_train)
+    predictions1 = model.predict(X_test)
+    predictions2 = MyModel.predict(X_test)
+    sensitivity1, specificity1, F1_score1 = evaluate(y_test, predictions1)
+    sensitivity2, specificity2, F1_score2 = evaluate(y_test, predictions2)
 
     # Print results
-    print(f"Correct: {(y_test == predictions).sum()}")
-    print(f"Incorrect: {(y_test != predictions).sum()}")
-    print(f"True Positive Rate: {100 * sensitivity:.2f}%")
-    print(f"True Negative Rate: {100 * specificity:.2f}%")
+    print(f"Corrects of sklearn KNN model: {(y_test == predictions1).sum()}")
+    print(f"Incorrects of sklearn KNN model: {(y_test != predictions1).sum()}")
+    print(f"Sensitivity of sklearn KNN model rate: {100 * sensitivity1:.2f}%")
+    print(f"Specificity of sklearn KNN model rate: {100 * specificity1:.2f}%")
+    print(f"F1 score of sklearn KNN model: {100 * F1_score1:.2f}%")
+    print(f"Corrects of My KNN model: {(y_test == predictions2).sum()}")
+    print(f"Incorrects of My KNN model: {(y_test != predictions2).sum()}")
+    print(f"Sensitivity of My KNN model rate: {100 * sensitivity2:.2f}%")
+    print(f"Specificity of My KNN model rate: {100 * specificity2:.2f}%")
+    print(f"F1 score of My KNN model: {100 * F1_score2:.2f}%")
 
 
 def load_data(filename):
@@ -103,9 +112,11 @@ def train_model(evidence, labels):
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
     model = KNeighborsClassifier(n_neighbors=1)
+    MyModel = KNearestNeighbors(k=1)
     model.fit(evidence, labels)
+    MyModel.fit(evidence, labels)
 
-    return model
+    return model, MyModel
 
 
 def evaluate(labels, predictions):
@@ -123,11 +134,12 @@ def evaluate(labels, predictions):
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-    sensitivity = float(0)
-    specificity = float(0)
+    sensitivity = float(0)  # True positives
+    specificity = float(0)  # True negatives
+    precision = float(0)
 
-    total_positive = float(0)
-    total_negative = float(0)
+    total_positive = float(0)   #Total actual positives
+    total_negative = float(0)   #Total actual negatives
 
     for label, prediction in zip(labels, predictions):
 
@@ -140,11 +152,18 @@ def evaluate(labels, predictions):
             total_negative += 1
             if label == prediction:
                 specificity += 1
+                
+        if prediction == 1:
+            precision += (label == prediction)
 
     sensitivity /= total_positive
     specificity /= total_negative
+    precision /= sum(predictions)
+    recall = sensitivity
+    
+    F1_score = 2 * (precision * recall) / (precision + recall)
 
-    return sensitivity, specificity
+    return sensitivity, specificity, F1_score
 
 
 if __name__ == "__main__":
